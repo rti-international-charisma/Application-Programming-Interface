1a4b95c7bcee96250c60b66b6f7de22f610ee99a
tejaswita <ttakawale@equalexperts.com>
2021-03-03 01:25:43 +0530

    adding in cdaassets

--- src/model/HomePage.kt
+++ src/model/HomePage.kt
@@ -1,4 +1,28 @@
 package com.rti.charisma.api.model
 
+import com.contentful.java.cda.CDAAsset
+import com.contentful.java.cda.CDAEntry
+import java.util.*
+import java.util.stream.Collector
+import java.util.stream.Collectors
+
 data class HomePage(val title: String, val contentBody: String, val heroImage: List<Asset>, val videos: List<Asset>)
-data class Asset(val id: String, val linkType: String)
+data class Asset(val id: String, val title: String, val url: String, val mimeType: String)
+
+
+fun converter(entry: CDAEntry) : HomePage  {
+    val title = entry.getField("title") as String
+    val contentBody = entry.getField("contentBody") as String
+    val heroImage = toAsset(entry.getField("heroImage") as List<CDAAsset>)
+    val videos  =  toAsset(entry.getField("videos") as List<CDAAsset>)
+
+    return HomePage(title, contentBody, heroImage, videos)
+}
+
+private fun toAsset(cdaAssets: List<CDAAsset>) : List<Asset> {
+    return cdaAssets
+        .stream()
+        .map { entry -> Asset(entry.id(), entry.title(), entry.url(), entry.mimeType()) }
+        .collect(Collectors.toList())
+
+}
--- src/service/ContentService.kt
+++ src/service/ContentService.kt
@@ -3,7 +3,7 @@ package com.rti.charisma.api.content
 import com.contentful.java.cda.CDAClient
 import com.contentful.java.cda.CDAEntry
 import com.rti.charisma.api.model.HomePage
-import java.util.*
+import com.rti.charisma.api.model.converter
 
 class ContentService {
     var client = CDAClient.builder()
@@ -11,31 +11,16 @@ class ContentService {
         .setSpace("5lkmroeaw7nj")
         .build()
 
-    fun getHomePage(): HomePage {
+    //TODO: set locale
 
-
-//        val homePage : HomePage = client.observeAndTransform(HomePage::class.java)
-//            .where("fields.pageid", "charisma-home")
-//            .include(5)
-//            .limit(1)
-//            .all()
-//            .blockingFirst()
-//            .first()
-//        return homePage
-
-        val homePage : CDAEntry = client.fetch(CDAEntry::class.java)
+    fun getHomePage(): HomePage{
+        val homePageEntry : CDAEntry = client.fetch(CDAEntry::class.java)
             .withContentType("homePage")
             .where("fields.pageid", "charisma-home")
             .include(5)
             .all()
             .items()
             .first() as CDAEntry
-
-
-        return HomePage(
-            homePage.getField("title") as String,
-            homePage.getField("contentBody") as String,
-            Collections.emptyList(),
-            Collections.emptyList())
+        return converter(homePageEntry);
     }
 }
